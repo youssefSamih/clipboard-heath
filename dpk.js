af11289f400b7114f492d7f638468bc234f87c3b
Youssef samih <youssef.samih97@gmail.com>
2022-11-23 02:17:51 +0100

    feat: add unit test

--- dpk.js
+++ dpk.js
@@ -1,28 +1,35 @@
 const crypto = require("crypto");
 
+// Constants
+
+const TRIVIAL_PARTITION_KEY = "0";
+const MAX_PARTITION_KEY_LENGTH = 256;
+
+// Helpers
+
+const createHash = (data) => {
+  return crypto.createHash("sha3-512").update(data).digest("hex");
+};
+
+const stringifyJson = (event) => {
+  return typeof event === "string" ? event : JSON.stringify(event);
+};
+
+// Exports
+
 exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
-
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  if (!event) {
+    return TRIVIAL_PARTITION_KEY;
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
+  const partitionKey = event.partitionKey
+    ? stringifyJson(event.partitionKey)
+    : createHash(stringifyJson(event));
+
+  const candidate =
+    partitionKey.length > MAX_PARTITION_KEY_LENGTH
+      ? createHash(partitionKey)
+      : partitionKey;
+
   return candidate;
 };
--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,23 @@
 const { deterministicPartitionKey } = require("./dpk");
 
 describe("deterministicPartitionKey", () => {
-  it("Returns the literal '0' when given no input", () => {
+  it("Should Returns the literal '0' when given no input", () => {
     const trivialKey = deterministicPartitionKey();
+
     expect(trivialKey).toBe("0");
+  });
+
+  it("Should ignore (event.partitionKey) that more than max length allowed", () => {
+    const partitionKey = "hello world!".repeat(300);
+    const keyExpected =
+      "6955dff255d95bfb11b02b395e03d2046a36f9a83401510fcce3d2beb103085ecb53183765ea663aae0fe4eddf75f1a3abeccc52e40d120acde60d06055a3330";
+
+    expect(deterministicPartitionKey({ partitionKey })).toBe(keyExpected);
+  });
+
+  it("Should return partition key provided in event (property partitionKey)", () => {
+    const partitionKey = "example-key";
+
+    expect(deterministicPartitionKey({ partitionKey })).toBe(partitionKey);
   });
 });
